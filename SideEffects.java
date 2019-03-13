4d3b29c7ff67c86a34a07e2d9c31cc66f99549e3
Sahira1234 <48065774+Sahira1234@users.noreply.github.com>
2019-03-14 01:34:36 +0500

    git commit

---
+++ SideEffects.java
@@ -0,0 +1,20 @@
+public class SideEffects {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i;
+        i=0;
+        if(false&(++i<100))
+            System.out.println("this would not be displayed");
+        System.out.println("if statement executs:"+i);
+        if(false&&(++i<100))
+            System.out.println("this would not be displayed");
+        System.out.println("if statement executs:"+i);
+            
+            
+        // TODO code application logic here
+    }
+    
+}
