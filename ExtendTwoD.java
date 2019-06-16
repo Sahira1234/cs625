1ab263d1d079140e133a8fb0edad4647dce66cd2
Sahira1234 <48065774+Sahira1234@users.noreply.github.com>
2019-06-17 03:12:59 +0500

    git commit

---
+++ ExtendTwoD.java
@@ -0,0 +1,10 @@
+class Rectangle extends TwoDShape { 
+  boolean isSquare() { 
+    if(width == height) return true; 
+    return false; 
+  } 
+   
+  double area() { 
+    return width * height; 
+  } 
+}
